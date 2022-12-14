89d75276e3695aa2dc06efd3ff055965a94feeaa
VishuPS <117966861+VishuPS@users.noreply.github.com>
2022-12-15 02:30:56 +0530

    Do not change

---
+++ SNRCal.m
@@ -0,0 +1,23 @@
+
+function SNRCal = SNRCal(bitcount)
+
+signal_i=rand(1,bitcount)>0.5 %Generation of the data bits
+
+%BPSK Modulated i component of the signal
+BPSK=2*signal_i-1
+
+
+%q component of the signal
+signal_q=zeros(1,bitcount)
+
+%Complete signal
+signal = complex(signal_i,signal_q)
+noise = 1/sqrt(2)*(randn(1,bitcount) + j*randn(1,bitcount))
+
+  signal_power = mean(abs(signal).^2)
+  noise_power = mean(abs(noise).^2)
+
+  SNR = 10*log10(signal_power/noise_power)
+
+  endfunction
+
