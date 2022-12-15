dfd958dfc1d87414d480c24b5110d3425d0dd196
VishuPS <117966861+VishuPS@users.noreply.github.com>
2022-12-15 14:05:19 +0530

    Updated SNRCal function file

--- SNRCal.m
+++ SNRCal.m
@@ -1,14 +1,13 @@
+function [SNRValue , BERValue] = SNRCal(bitcount)
 
-function SNRCal = SNRCal(bitcount)
-
-signal_i=rand(1,bitcount)>0.5 %Generation of the data bits
+signal_i=rand(1,bitcount)>0.5; %Generation of the data bits
 
 %BPSK Modulated i component of the signal
-BPSK=2*signal_i-1
+BPSK=2*signal_i-1;
 
 
 %q component of the signal
-signal_q=zeros(1,bitcount)
+signal_q=zeros(1,bitcount);
 
 %Complete signal
 signal = complex(signal_i,signal_q)
@@ -17,7 +16,9 @@ noise = 1/sqrt(2)*(randn(1,bitcount) + j*randn(1,bitcount))
   signal_power = mean(abs(signal).^2)
   noise_power = mean(abs(noise).^2)
 
-  SNR = 10*log10(signal_power/noise_power)
+  SNRValue = 10*log10(signal_power/noise_power); %SNR Calculation done in dB
 
-  endfunction
+  BERValue = 0.5*(1-(sqrt(SNRValue/2+SNRValue)));
+
+endfunction
 
